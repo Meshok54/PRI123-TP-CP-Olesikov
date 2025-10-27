b201487a93f521f1ab3b1100c7518f935bcf9b23
Meshok54 <mishaoles@gmail.com>
2025-10-27 14:28:53 +0300

    Добавьте файлы проекта.

---
+++ Cadastral Management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cadastral_Management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
