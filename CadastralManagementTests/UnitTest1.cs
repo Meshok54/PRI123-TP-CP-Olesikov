c743f7752ac4baf9089652ddc0c08a03eb48007d
Meshok54 <mishaoles@gmail.com>
2025-12-22 17:58:37 +0300

    Создан тест-проект

---
+++ CadastralManagementTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CadastralManagementTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
